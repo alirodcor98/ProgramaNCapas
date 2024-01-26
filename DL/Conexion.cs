a0f09371b6bcc6a5cd69d2154db3e62f1f21d639
Alí Rodríguez Cortés <92193598+alirodcor98@users.noreply.github.com>
2024-01-26 12:14:27 -0600

    Add files via upload

---
+++ DL/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Conexion
+    {
+        public static string ObtenerConnectionString()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.ConnectionStrings["ARodriguezProgramacionNCapas"].ToString();
+            return cadenaConexion;
+        }
+    }
+}
