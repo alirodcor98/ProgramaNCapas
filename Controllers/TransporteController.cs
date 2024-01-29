5fbcf8a775bdf8dd6b169a3b92608b6c54716698
Alí Rodríguez Cortés <92193598+alirodcor98@users.noreply.github.com>
2024-01-29 17:47:12 -0600

    Add files via upload

--- Controllers/TransporteController.cs
+++ Controllers/TransporteController.cs
@@ -1,134 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.UI.WebControls;
-using System.Web.WebSockets;
-using WebGrease.Css.Ast;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
-namespace PL_MVC.Controllers
+namespace SL_WebApi.Controllers
 {
-    public class TransporteController : Controller
+    public class TransporteController : ApiController
     {
-        // GET: Transporte
         [HttpGet]
-        public ActionResult GetAll()
+        [Route("api/Transporte/GetAll")]
+        public IHttpActionResult GetAll()
         {
-            ServiceTransporte.ServiceTransporteClient serviceTransporte = new ServiceTransporte.ServiceTransporteClient();
-            var result = serviceTransporte.GetAll();
-            //Dictionary<string, object> result = serviceTransporte.GetAll();
-            //Dictionary<string, object> result = BL.Transporte.GetAll();
-            bool resultado = result.Resultado;
-            if(resultado == true)
+            Dictionary <string, object> result = BL.Transporte.GetAll();
+            bool resultado = (bool)result["Resultado"];
+            if (resultado)
             {
-                ML.Transporte transporte = new ML.Transporte();
-                transporte.Transportes = result.Objects.ToList();
-                return View(transporte);
+                return Ok(result);
             }
             else
             {
-                string excepcion = result.Mensaje;
-                return View();
-            }
-        }
-        [HttpGet]
-        public ActionResult Form(int? idTransporte)
-        {
-            Dictionary<string, object> resultEstatus = BL.EstatusTransporte.GetAll();
-            bool correctEstatus = (bool)resultEstatus["Resultado"];
-            ML.EstatusTransporte estatus = (ML.EstatusTransporte)resultEstatus["Estatus"];
-            ML.Transporte transporte = new ML.Transporte();
-            transporte.Estatus = new ML.EstatusTransporte();
-            transporte.Estatus.EstatusTransportes = estatus.EstatusTransportes;
-            if(idTransporte != null)
-            {
-                ServiceTransporte.ServiceTransporteClient serviceTransporte = new ServiceTransporte.ServiceTransporteClient();
-                var result = serviceTransporte.GetById(idTransporte.Value);
-                //Dictionary<string, object> result = serviceTransporte.GetById(idTransporte.Value);
-                //Dictionary<string, object> result = BL.Transporte.GetById(idTransporte.Value);
-                bool resultado = result.Resultado;
-                if(resultado == true && correctEstatus == true)
-                {
-                    transporte = (ML.Transporte)result.Object;
-                    transporte.Estatus = new ML.EstatusTransporte();
-                    transporte.Estatus.EstatusTransportes = estatus.EstatusTransportes;
-                    return View(transporte);
-                }
-                else
-                {
-                    string excepcion = result.Mensaje;
-                    ViewBag.Mensaje = "Ocurrio una excepcion inesperada: " + excepcion;
-                    return PartialView("Modal");
-                }
-            }
-            else
-            {
-                return View(transporte);
-            }
-        }
-        [HttpPost]
-        public ActionResult Form(ML.Transporte transporte)
-        {
-            if(transporte.IdTransporte > 0)
-            {
-                //Update
-                ServiceTransporte.ServiceTransporteClient serviceTransporte = new ServiceTransporte.ServiceTransporteClient();
-                var result = serviceTransporte.Update(transporte);
-                //Dictionary<string, object> result = serviceTransporte.Update(transporte);
-                //Dictionary<string, object> result = BL.Transporte.Update(transporte);
-                bool resultado = result.Resultado;
-                if(resultado == true)
-                {
-                    ViewBag.Mensaje = "Se ha actualizado correctamente este registro";
-                    return PartialView("Modal");
-                }
-                else
-                {
-                    string excepcion = result.Mensaje;
-                    ViewBag.Mensaje = "Ha ocurrido una excepcion inesperada: " + excepcion;
-                    return PartialView("Modal");
-                }
-            }
-            else
-            {
-                //Add
-                ServiceTransporte.ServiceTransporteClient serviceTransporte = new ServiceTransporte.ServiceTransporteClient();
-                var result = serviceTransporte.Add(transporte);
-                //Dictionary<string, object> result = serviceTransporte.Add(transporte);
-                //Dictionary<string, object> result = BL.Transporte.Add(transporte);
-                bool resultado = result.Resultado;
-                if(resultado == true)
-                {
-                    ViewBag.Mensaje = "Se ha agregado el registro correctamente";
-                    return PartialView("Modal");
-                }
-                else
-                {
-                    string excepcion = result.Mensaje;
-                    ViewBag.Mensaje = "Ha ocurrido una excepcion inesperada: " + excepcion;
-                    return PartialView("Modal");
-                }
+                return BadRequest("Error al obtener los datos");
             }
         }
 
-        public ActionResult Delete(int idTransporte)
+        [HttpGet]
+        [Route("api/Transporte/GetById[idTransporte]")]
+        public IHttpActionResult GetById(int idTransporte)
         {
-            ServiceTransporte.ServiceTransporteClient serviceTransporte = new ServiceTransporte.ServiceTransporteClient();
-            var result = serviceTransporte.Delete(idTransporte);
-            //Dictionary<string,object> result = serviceTransporte.Delete(idTransporte);
-            //Dictionary<string, object> result = BL.Transporte.Delete(idTransporte);
-            bool resultado = result.Resultado;
-            if(resultado == true)
+            Dictionary<string, object> result = BL.Transporte.GetById(idTransporte);
+            bool resultado = (bool)result["Resultado"];
+            if (resultado)
             {
-                ViewBag.Mensaje = "Se ha borrado el registeo exitosamente";
-                return PartialView("Modal");
+                return Ok(result);
             }
             else
             {
-                string excepcion = result.Mensaje;
-                ViewBag.Mensaje = "Ha ocurrido una excepcion inesperada: " + excepcion;
-                return PartialView("Modal");
+                return BadRequest("Error al obtener los datos");
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/Transporte/Delete[idTransporte]")]
+        public IHttpActionResult Delete(int idTransporte)
+        {
+            Dictionary<string, object> result = BL.Transporte.Delete(idTransporte);
+            bool resultado = (bool)result["Resultado"];
+
+            if (resultado)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("Error al borrar el registro");
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Transporte/Add")]
+        public IHttpActionResult Add([FromBody]ML.Transporte transporte)
+        {
+            Dictionary<string, object> result = BL.Transporte.Add(transporte);
+            bool resultado = (bool)result["Resultado"];
+            if (resultado)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("Error al agregar el registro");
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Transporte/Update")]
+        public IHttpActionResult Update([FromBody]ML.Transporte transporte)
+        {
+            Dictionary<string,object> result = BL.Transporte.Update(transporte);
+            bool resultado = (bool)result["Resultado"];
+            if (resultado)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest("Error al actualizar el registro");
             }
         }
     }
